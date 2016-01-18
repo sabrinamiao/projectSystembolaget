a3964c791c495f48d7c128574872dd5780ca751a
sabrinamiao <sabrinamsh@gmail.com>
2016-01-18 16:26:20 +0100

    update
    ng-repeat still not working, no data display on html

--- systembolaget/data.js
+++ systembolaget/data.js
@@ -1,9 +1,7 @@
-var app= angular.module("myApp", []);
+var app= angular.module('myApp', []);
 
-app.controller('getSortiment', function($scope, $http){
-    $http.get('api.php')
-        .then(function(response) {
-            // here the data from the api is assigned to a variable named users
-            $scope.sortiments =response.data.records;
+app.controller('sortCtrl', function($scope, $http){
+    $http.get('api.php').then(function(data){
+        $scope.sortiments = data;
         });
 });
