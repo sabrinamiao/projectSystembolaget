d0593eaf0954f09e68a3a281a92cc57d06c14508
sabrinamiao <sabrinamiao@users.noreply.github.com>
2016-01-21 17:35:30 +0100

    Update
    insert a line to make filter working

--- systembolaget/data.js
+++ systembolaget/data.js
@@ -1,13 +1,16 @@
-var app= angular.module('myApp', []);
+var app = angular.module('myApp', []);
 
-app.controller('sortCtrl', ['$scope','$http',function($scope, $http){
-    
-$http.get('api.php')
-    .then(function(data){
-        $scope.sortiments = data.data.records;
-        //console.log(data);
-    })
-    .catch(function (err){
-        console.log(err);
-    });
+//Get data from JSON
+app.controller('sortController', ['$scope', '$http', function ($scope, $http) {
+
+
+    $http.get('../php/api.php')
+        .success(function (data) {
+            $scope.search = [];
+            $scope.sortiments = data;
+        }).error(function (data, status, headers, config) {
+            console.log('error');
+        });
+
+
 }]);
